7ab3fe7e143d0a9b5ebdf91d98e9119f4cd1a0f5
benzproduction <sven.huepers@pwc.com>
2023-01-25 15:27:05 +0100

    +script to get years for stations

--- data/station_to_json.py
+++ data/station_to_json.py
@@ -3,6 +3,9 @@ import json
 import os
 from tqdm import tqdm
 import warnings
+import urllib.request
+import gzip
+import shutil
 
 # ignore warnings
 pd.options.mode.chained_assignment = None
@@ -12,43 +15,90 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 # change the working directory to the current directory
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-# create an empty dataframe to store the station metadata
-station_metadata = pd.DataFrame(columns=['station_id', 'latitude', 'longitude', 'elevation', 'country_code', 'country_name', 'name'])
+def download_file(name):
+    url = f"https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/by_station/{name}.csv.gz"
+    file_name = f"{name}.csv.gz"
 
-# run over the data/ghcnd-stations.txt and set a new row based on the string in each line
-# eample line: AE000041196  25.3330   55.5170   34.0    SHARJAH INTER. AIRP            GSN     41196
+    try:
+        urllib.request.urlretrieve(url, file_name)
+        print(f"File {file_name} downloaded successfully.")
+        return file_name
+    except urllib.error.HTTPError as e:
+        if e.code == 404:
+            print(f"Error: {name} not found.")
+        else:
+            print(f"Error: {e.reason}")
+        exit()
+    except Exception as e:
+        print(f"Error: {e}")
+        exit()
+
+def read_file(file_name):
+    # open and read the gzipped file then remove the original file
+    with gzip.open(file_name, 'rb') as f_in, open(file_name[:-3], 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
+    os.remove(file_name)
+    return file_name[:-3]
+
+# read in the station metadata
+df = pd.read_csv('data/station_metadata3.csv', sep=',', header=0)
+
+# for every stationId request the csv file from 
+# https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/by_station/<stationId>.csv.gz
+# and get a list of all years for which data is available
+interator = df.iterrows()
+
+for i, row in tqdm(interator):
+    stationId = row['station_id']
+    try: 
+        if type(row['years']) == str:
+            continue
+    except:
+        pass
+    file_name = download_file(stationId)
+    file_name = read_file(file_name)
+    df2 = pd.read_csv(file_name, sep=',', header=None, names=['station_id', 'date', 'element', 'value', 'm_flag', 'q_flag', 's_flag', 'obs_time'])
+    df2 = df2[df2['element'].isin(['TMAX', 'TMIN'])]
+    years = df2['date'].astype(str).str[:4].unique()
+    df.at[i, 'years'] = years
+    os.remove(file_name)
+    # save the current state of the dataframe
+    df.to_csv('data/station_metadata3.csv', index=False)
+
+
+
+
+
+
+
+
+# # create an empty dataframe to store the station metadata
+# station_metadata = pd.DataFrame(columns=['station_id', 'latitude', 'longitude', 'elevation', 'country_code', 'country_name', 'name'])
+
+
+
+# # do the above faster with multiprocessing
+# def process_line(line):
+#     return {'station_id': line[:11].strip(),
+#             'latitude': float(line[12:20]),
+#             'longitude': float(line[21:30]),
+#             'elevation': float(line[31:37]),
+#             'name': line[41:71].strip(),
+#             'country_code': line[:2],
+#             }
+
+# # read in the data/ghcnd-stations.txt file
 # with open('data/ghcnd-stations.txt', 'r') as f:
-#     for line in tqdm(f):
-#         station_metadata = station_metadata.append({'station_id': line[:11].strip(),
-#                                                     'latitude': float(line[12:20]),
-#                                                     'longitude': float(line[21:30]),
-#                                                     'elevation': float(line[31:37]),
-#                                                     'name': line[41:71].strip(),
-#                                                     'country_code': line[:2],
-#                                                     }, ignore_index=True)
-
-# do the above faster with multiprocessing
-def process_line(line):
-    return {'station_id': line[:11].strip(),
-            'latitude': float(line[12:20]),
-            'longitude': float(line[21:30]),
-            'elevation': float(line[31:37]),
-            'name': line[41:71].strip(),
-            'country_code': line[:2],
-            }
-
-# read in the data/ghcnd-stations.txt file
-with open('data/ghcnd-stations.txt', 'r') as f:
-    lines = f.readlines()
+#     lines = f.readlines()
     
-    for line in tqdm(lines):
-        station_metadata = station_metadata.append(process_line(line), ignore_index=True)
+#     for line in tqdm(lines):
+#         station_metadata = station_metadata.append(process_line(line), ignore_index=True)
 
 
 
 
-# save as csv file  
-station_metadata.to_csv('data/station_metadata.csv', index=False)
+# # save as csv file  
+# station_metadata.to_csv('data/station_metadata.csv', index=False)
 
 
 # # Read in the data
