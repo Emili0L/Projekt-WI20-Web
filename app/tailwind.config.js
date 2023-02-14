900694945a76a80c2ce60d74354a206f050dc3ab
benzproduction <sven.huepers@pwc.com>
2023-02-14 14:54:04 +0100

    mobile view fixed

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
+    screens: {
+      'xs': '375px',
+      'ssm': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
   plugins: [
